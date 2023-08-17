81c2eca2dca433e799d511317e47ddae340d054d
NaharFreelancer <nahar.opediatech02@gmail.com>
2023-08-17 19:18:40 +0600

    update

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// JavaScript code to toggle mobile menu
+document.getElementById("menuIcon").addEventListener("click", function () {
+    document.querySelector(".menu__bar").classList.toggle("show-menu");
+});
